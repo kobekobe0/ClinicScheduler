400b3dbe007f20f2d43fb71529b6f1c95df172c8
kobekobe0 <72067397+kobekobe0@users.noreply.github.com>
2023-03-09 13:31:59 +0800

    Created schemas for appointments

---
+++ server/models/schema/appointmentSchema.js
@@ -0,0 +1,48 @@
+import { Schema } from 'mongoose'
+import { idGeneratorHelper } from '../../utils/helper.js'
+
+const dateSchema = new Schema({
+    time: {
+        type: Number,
+        required: true,
+    },
+    day: {
+        type: Number,
+        required: true,
+    },
+    year: {
+        type: Number,
+        required: true,
+    },
+})
+
+const appointmentSchema = new Schema({
+    _id: {
+        type: String,
+        required: true,
+        default: idGeneratorHelper('apt'),
+    },
+    updatedAt: Date,
+    createdAt: {
+        type: Date,
+        required: true,
+    },
+    doctorId: {
+        type: String,
+        required: true,
+    },
+    patientId: {
+        type: String,
+        required: true,
+    },
+    schedule: {
+        type: dateSchema,
+        required: true,
+    },
+    isComplete: {
+        type: Boolean,
+        default: false,
+    },
+})
+
+export default appointmentSchema
