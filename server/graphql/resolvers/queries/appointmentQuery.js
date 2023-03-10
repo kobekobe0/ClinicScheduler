2d7b2272b56aeca26c929c050358f34c0cd98de7
kobekobe0 <72067397+kobekobe0@users.noreply.github.com>
2023-03-10 12:09:33 +0800

    Created 'allAppointments' mutation

--- server/graphql/resolvers/queries/appointmentQuery.js
+++ server/graphql/resolvers/queries/appointmentQuery.js
@@ -4,7 +4,7 @@ import { GraphQLError } from 'graphql'
 import * as dotenv from 'dotenv'
 dotenv.config()
 import bcrypt from 'bcryptjs'
-import { USER_PER_PAGE } from '../../../utils/constants.js'
+import { APPOINTMENT_PER_PAGE } from '../../../utils/constants.js'
 import Appointments from '../../../models/Appointment.js'
 
 const userAppointment = async (parent, { appointmentId }, { token }) => {
@@ -70,7 +70,7 @@ const userAppointment = async (parent, { appointmentId }, { token }) => {
     }
 }
 
-const userAppointments = async (parent, _, { token }) => {
+const userAppointments = async (parent, { page }, { token }) => {
     try {
         console.log('userAppointments query: started')
 
@@ -89,6 +89,8 @@ const userAppointments = async (parent, _, { token }) => {
         const appointments = await Appointments.find({
             $or: [{ patientId: auth.userId }, { doctorId: auth.userId }],
         })
+            .skip(page * APPOINTMENT_PER_PAGE)
+            .limit(APPOINTMENT_PER_PAGE)
         if (!appointments) {
             throw new GraphQLError('Cannot find the appointment', {
                 extensions: {
@@ -127,4 +129,71 @@ const userAppointments = async (parent, _, { token }) => {
     }
 }
 
-export { userAppointment, userAppointments }
+const allAppointments = async (parent, { page }, { token }) => {
+    try {
+        console.log('userAppointments query: started')
+
+        const auth = checkAuth(token)
+        if (!auth) {
+            throw new GraphQLError(
+                'You are not authorized to perform this action.',
+                {
+                    extensions: {
+                        code: 'FORBIDDEN',
+                    },
+                }
+            )
+        }
+        if (auth.email != process.env.ADMIN_EMAIL) {
+            throw new GraphQLError(
+                'You are not authorized to perform this action. Admin level access.',
+                {
+                    extensions: {
+                        code: 'FORBIDDEN',
+                    },
+                }
+            )
+        }
+
+        const appointments = await Appointments.find()
+            .skip(page * APPOINTMENT_PER_PAGE)
+            .limit(APPOINTMENT_PER_PAGE)
+        if (!appointments) {
+            throw new GraphQLError('Cannot find the appointment', {
+                extensions: {
+                    code: 'INVALID ID',
+                },
+            })
+        }
+
+        let queriedAppointments = []
+
+        appointments.forEach((appointment) => {
+            queriedAppointments.push({
+                appointmentId: appointment?._id,
+                doctorId: appointment?.doctorId,
+                patientId: appointment?.patientId,
+                schedule: `${appointment?.schedule?.day}/${appointment?.schedule?.month}/${appointment?.schedule?.year}`,
+                time: `${appointment?.schedule?.time}`,
+                room: appointment?.room,
+                isComplete: appointment?.isComplete,
+                isCancelled: appointment?.isCancelled,
+            })
+        })
+
+        return queriedAppointments
+    } catch (error) {
+        console.error('userAppointments: exception occurred', {
+            errorMessage: error?.message,
+            details: { error },
+        })
+        throw new GraphQLError(`See Errors: ${error?.message}`, {
+            extensions: {
+                code: error?.extensions?.code || 'SCHEDULER_QUERY_ERROR',
+            },
+            originalError: error,
+        })
+    }
+}
+
+export { userAppointment, userAppointments, allAppointments }
