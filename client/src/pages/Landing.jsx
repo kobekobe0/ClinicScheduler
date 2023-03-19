b0eec45166649222dc2d2af2ebe9164967f99726
kobekobe0 <72067397+kobekobe0@users.noreply.github.com>
2023-03-19 21:47:10 +0800

    Somewhat responsive(not finished)

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -7,12 +7,12 @@ function Landing() {
         <div className="bg-dominant font-inter w-full h-fit text-3xl font-bold flex flex-col items-center">
             {' '}
             {/* top portion */}
-            <div className="flex flex-col w-1/2 items-center h-fit bg-dominant mb-60">
-                <div className="text-white flex py-10 w-full justify-between">
+            <div className="flex flex-col w-1/2 items-center h-fit bg-dominant mb-60 -xl:w-full">
+                <div className="text-white flex py-10 w-full justify-between -xl:px-12">
                     <div>
                         <h1>Startup</h1>
                     </div>
-                    <div className="w-fit">
+                    <div className="w-fit -md:hidden">
                         <ul className="font-normal flex text-xl gap-7">
                             <li>Careers</li>
                             <li>Services</li>
@@ -22,11 +22,11 @@ function Landing() {
                 </div>
                 <div className="w-full flex flex-col items-center mt-28">
                     <div className="flex flex-col items-center">
-                        <h2 className="text-white text-5xl">
+                        <h2 className="text-white text-5xl -xl:text-4xl">
                             Your health matters
                         </h2>
 
-                        <p className="text-white text-xl font-semibold text-center w-2/3 mt-5">
+                        <p className="text-white text-xl font-semibold text-center w-2/3 mt-5 -xl:font-normal -lg:text-lg">
                             Scheduling your healthcare appointments has never
                             been easier. With our user-friendly platform, you
                             can quickly and easily book appointments with your
@@ -36,11 +36,11 @@ function Landing() {
                     </div>
                     <div className="flex w-1/2 items-center justify-center mt-12 text-lg font-semibold">
                         <div className="flex items-center w-full justify-center">
-                            <div className="text-white gap-4 w-full flex items-center justify-center">
-                                <button className="px-6 py-3 bg-sub rounded-lg w-1/3">
+                            <div className="text-white gap-4 w-full flex items-center justify-center -lg:flex-col -md:text-base">
+                                <button className="px-6 py-3 bg-sub rounded-lg w-fit">
                                     Get Started
                                 </button>
-                                <button className="px-6 py-3 bg-minor rounded-lg w-1/3">
+                                <button className="px-6 py-3 bg-minor rounded-lg w-fit">
                                     Sign in
                                 </button>
                             </div>
@@ -52,13 +52,13 @@ function Landing() {
             <div className="h-fit w-full bg-lighterDominant justify-center flex text-white pb-32">
                 <div className="w-1/2 flex flex-col items-center">
                     <div className="flex flex-col justify-center items-center mt-24">
-                        <h2 className="text-5xl">Main Features</h2>
-                        <h3 className="text-xl font-normal text-lightGray mt-5">
+                        <h2 className="text-5xl -lg:text-2xl">Main Features</h2>
+                        <h3 className="text-xl font-normal text-lightGray mt-5 -lg:text-lg -lg:text-center">
                             Things you can do with the help of our system.
                         </h3>
                     </div>
-                    <div className="flex mt-24 gap-20 justify-center">
-                        <div className=" flex flex-col justify-center items-center text-center w-2/5">
+                    <div className="flex mt-24 gap-20 justify-center -lg:flex-col">
+                        <div className=" flex flex-col justify-center items-center text-center w-2/5 -lg:w-full">
                             <div className="p-4 w-fit bg-minor rounded-lg mb-6">
                                 <svg
                                     xmlns="http://www.w3.org/2000/svg"
@@ -71,13 +71,13 @@ function Landing() {
                                     <path d="M8 1a5 5 0 0 0-5 5v1h1a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V6a6 6 0 1 1 12 0v6a2.5 2.5 0 0 1-2.5 2.5H9.366a1 1 0 0 1-.866.5h-1a1 1 0 1 1 0-2h1a1 1 0 0 1 .866.5H11.5A1.5 1.5 0 0 0 13 12h-1a1 1 0 0 1-1-1V8a1 1 0 0 1 1-1h1V6a5 5 0 0 0-5-5z" />
                                 </svg>
                             </div>
-                            <h3>Online Booking</h3>
-                            <p className="text-lg font-normal">
+                            <h3 className="-lg:text-2xl">Online Booking</h3>
+                            <p className="text-lg font-normal -lg:text-base mt-4">
                                 Patients can view available appointments and
                                 schedule their appointments online at any time.
                             </p>
                         </div>
-                        <div className=" flex flex-col justify-center items-center text-center w-2/5">
+                        <div className=" flex flex-col justify-center items-center text-center w-2/5 -lg:w-full">
                             <div className="p-4 w-fit bg-minor rounded-lg mb-6">
                                 <svg
                                     xmlns="http://www.w3.org/2000/svg"
@@ -92,8 +92,10 @@ function Landing() {
                                     <path d="M1 5v-.5a.5.5 0 0 1 1 0V5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0V8h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0v.5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1z" />
                                 </svg>
                             </div>
-                            <h3>Appointment reminders</h3>
-                            <p className="text-lg font-normal">
+                            <h3 className="-lg:text-2xl">
+                                Appointment reminders
+                            </h3>
+                            <p className="text-lg font-normal mt-4 -lg:text-base">
                                 Automated reminders via email, text message, or
                                 phone call.
                             </p>
@@ -103,21 +105,24 @@ function Landing() {
             </div>
             {/*third portion*/}
             <div className="h-fit w-full bg-dominant justify-center flex text-white">
-                <div className="w-1/2 flex flex-col items-center">
+                <div className="w-1/2 flex flex-col items-center -lg:w-full -lg:px-12">
                     <div className="flex flex-col justify-center items-center mt-24">
-                        <h2 className="text-5xl">For professionals</h2>
-                        <h3 className="text-xl font-normal text-lightGray mt-5">
+                        <h2 className="text-5xl -lg:text-2xl">
+                            For professionals
+                        </h2>
+                        <h3 className="text-xl font-normal text-lightGray mt-5 -lg:text-center -lg:text-lg">
                             We are currently looking for:
                         </h3>
                     </div>
                     <div className="mt-10 bg-lighterDominant p-5 rounded-lg">
-                        <div className="flex align-center justify-center h-fit">
+                        <div className="flex align-center justify-center h-fit -md:flex-col gap-5 -md:items-center -md:text-center">
                             <img
                                 src={consultant}
                                 alt="consultant"
-                                style={{ width: '200px', height: '200px' }}
+                                style={{ width: '140px', height: 'auto' }}
+                                className=" object-contain"
                             />
-                            <div className="justify-center flex flex-col">
+                            <div className="justify-center flex flex-col gap-5">
                                 <h4 className="text-2xl">Junior consultants</h4>
                                 <p className="text-base font-normal">
                                     A junior consultant is an entry-level
@@ -131,9 +136,15 @@ function Landing() {
                             </div>
                         </div>
                     </div>
-                    <div className="mt-10 bg-lighterDominant p-5 rounded-lg text-end">
-                        <div className="flex align-center justify-center h-fit">
-                            <div className="justify-center flex flex-col mr-4">
+                    <div className="mt-10 bg-lighterDominant p-5 rounded-lg text-start">
+                        <div className="flex align-center justify-center h-fit -md:flex-col gap-5 -md:items-center -md:text-center">
+                            <img
+                                src={surgeon}
+                                alt="consultant"
+                                style={{ width: '140px', height: 'auto' }}
+                                className="object-contain"
+                            />
+                            <div className="justify-center flex flex-col mr-4 gap-5">
                                 <h4 className="text-2xl">Surgeons</h4>
                                 <p className="text-base font-normal">
                                     Surgeons perform surgeries to treat
@@ -144,21 +155,17 @@ function Landing() {
                                     hospitals and clinics.
                                 </p>
                             </div>
-                            <img
-                                src={surgeon}
-                                alt="consultant"
-                                style={{ width: '200px', height: '200px' }}
-                            />
                         </div>
                     </div>
                     <div className="mt-10 bg-lighterDominant p-5 rounded-lg">
-                        <div className="flex align-center justify-center h-fit">
+                        <div className="flex align-center justify-center h-fit -md:flex-col gap-5 -md:items-center -md:text-center">
                             <img
                                 src={physician}
                                 alt="consultant"
-                                style={{ width: '200px', height: '200px' }}
+                                style={{ width: '140px', height: 'auto' }}
+                                className="object-contain"
                             />
-                            <div className="justify-center flex flex-col">
+                            <div className="justify-center flex flex-col gap-5">
                                 <h4 className="text-2xl">Physicians</h4>
                                 <p className="text-base font-normal">
                                     Physicians are medical professionals who
@@ -172,22 +179,23 @@ function Landing() {
                             </div>
                         </div>
                     </div>
-                    <div className="flex p-12 text-lightGray mb-10">
-                        <h3 className="text-lg font-medium">
-                            You may send your CVs here{' '}
+                    <div className="flex p-12 text-lightGray mb-10 -lg:flex-col text-center">
+                        <h3 className="text-lg font-medium -lg:text-sm -md:text-xs">
+                            You may send your CVs here :
                         </h3>
-                        <i className="text-lg font-medium">
-                            : kobebrian.santos.e@gmail.com{' '}
+                        <i className="text-lg font-medium -lg:text-sm">
+                            {' '}
+                            kobebrian.santos.e@gmail.com{' '}
                         </i>
                     </div>
                 </div>
             </div>
             {/*fourth*/}
             <div className="h-screen w-full justify-center flex text-white bg-lighterDominant">
-                <div className="w-1/2 flex flex-col items-center">
+                <div className="w-1/2 flex flex-col items-center -lg:w-full -lg:text-center -lg: px-5">
                     <div className="flex flex-col justify-center items-center mt-24">
-                        <h2 className="text-5xl">About</h2>
-                        <h3 className="text-xl font-normal text-lightGray mt-5">
+                        <h2 className="text-5xl -lg:text-2xl">About</h2>
+                        <h3 className="text-xl font-normal text-lightGray mt-5 -lg:text-lg">
                             Who are we?
                         </h3>
                     </div>
@@ -204,8 +212,8 @@ function Landing() {
                             className="bg-dominant p-2"
                         />
                     </div>
-                    <div>
-                        <p className="text-lg font-normal mt-10">
+                    <div className="h-full">
+                        <p className="text-lg font-normal  -lg:text-base">
                             Our hospital scheduling website is a platform
                             designed to make healthcare scheduling easier and
                             more convenient for patients and healthcare
@@ -238,7 +246,6 @@ function Landing() {
                     </div>
                 </div>
             </div>
-            {/*footer*/}
             <div className="h-fit w-full justify-center flex text-white bg-dominant">
                 <div className="p-24">
                     <h5 className="text-lg font-semibold">
